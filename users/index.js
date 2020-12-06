3001df0799d6f4752de0020f8b7dcb07efe2de20
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-12-06 15:45:00 +0530

    fix: callback and host url path striped

--- users/index.js
+++ users/index.js
@@ -45,7 +45,7 @@ passport.use(
     {
       clientID: process.env.GH_CLIENT_ID,
       clientSecret: process.env.GH_CLIENT_SECRET,
-      callbackURL: `${callbackUrl}/auth/callback`,
+      callbackURL: callbackUrl,
       scope: [
         "repo:status",
         "read:org",
@@ -115,6 +115,10 @@ passport.use(
   )
 );
 
+/**
+ * @param {import("express").Request} req HTTP request context.
+ * @param {import("express").Response} res HTTP response context.
+ */
 exports.auth = async (req, res, next) => {
   passport.authenticate("github", (err, user, info) => {
     if (err) res.status(400).json({ body: "Login failed. Try again!" });
@@ -128,6 +132,10 @@ exports.auth = async (req, res, next) => {
   })(req, res, next);
 };
 
+/**
+ * @param {import("express").Request} req HTTP request context.
+ * @param {import("express").Response} res HTTP response context.
+ */
 exports.callback = async (req, res, next) => {
   const data = req.user;
   const token = Buffer.from(JSON.stringify(data)).toString("base64");
@@ -396,7 +404,7 @@ exports.editUser = async (req, res) => {
  * @param {import("express").Request} req HTTP request context.
  * @param {import("express").Response} res HTTP response context.
  */
-exports.verfiyUser = async (req, res) => {
+exports.verifyUser = async (req, res) => {
   console.log("timeuuid in createUser: " + myuuid);
 
   if (req.method !== "POST") {
